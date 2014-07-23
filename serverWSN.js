28dd1cb9a3e7281ac44a3176f759bed1508b58ab
r9hino <philippe.ilharreguy@gmail.com>
2014-07-23 18:38:01 -0400

    Add initialization module. JSON file is create using sync write-read functions.

--- serverWSN.js
+++ serverWSN.js
@@ -11,36 +11,12 @@ var bbb = require('bonescript');
 
 app.use(express.static(__dirname + '/public'));
 
-// This json will be loaded only if there doesn't exist an infoWSN.json file.
-// i.e. if it is the first time running the script, or if infoWSN.json was previewsly deleted
-var jsonWSN = {
-    "PB0": {"pin": "P8_10", "name": "Calentador Pipo", "value": 0},
-    "PB1": {"pin": "P8_11", "name": "Lampara Pipo", "value": 0}
-};
-
-// Configure pins as input or output
-bbb.pinMode(jsonWSN["PB0"].pin, bbb.OUTPUT);
-bbb.pinMode(jsonWSN["PB1"].pin, bbb.OUTPUT);
-
+// System state initialization
+var init = require('./init');
+// Create or restore system's state JSON file infoWSN.json
+var jsonWSN = init.initialization();
 var jsonFileName = __dirname + "/infoWSN.json";
-fs.readFile(jsonFileName, function(error, fileData) {
-    // If file doesn't exist, it will create one with an initial JSON
-    if(error){
-        console.log("File doesn't exist. It will be created now...");
-        fs.writeFile(jsonFileName, JSON.stringify(jsonWSN, null, 4), function(err) {
-            if(err) console.log(err);
-            else console.log("JSON saved to " + jsonFileName);
-        });
-    }
-    // If it does exist, load JSON file into a variable, and
-    // initialize system state from preview stored json file
-    else {
-        jsonWSN = JSON.parse(fileData);
-        for(var id in jsonWSN){
-            bbb.digitalWrite(jsonWSN[id].pin, jsonWSN[id].value);
-        }
-    }
-});
+
 
 io.sockets.on('connection', function (socket) {
     socket.on('buttonPress', updateSystemState);
